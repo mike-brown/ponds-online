9804429dfbee3ef66cc3801a4e31542aa262d4d9
Matt Barber <mbarber2@sheffield.ac.uk>
2017-06-22 15:09:29 +0100

    modified: altered loop code; fixed indexing issues

--- scripts/fluid.js
+++ scripts/fluid.js
@@ -1,19 +1,19 @@
 'use strict'
 
-const cols = 9
-const rows = 27
+const cols = 27
+const rows = 9
 
 let state = zeros(cols, rows)
 
 state[4][0] = 1
 
-let prevP = zeros(cols, rows) // I,J
-let prevX = zeros(cols + 1, rows) // i,J
-let prevY = zeros(cols, rows + 1) // I,j
+let prevP = zeros(cols, cols) //     (27C,  9R)
+let prevX = zeros(cols + 1, rows) // (28C,  9R)
+let prevY = zeros(cols, rows + 1) // (27C, 10R)
 
-prevX[4][4] = 0.00005
+prevX[6][2] = 0.00005
 
-let oldP = zeros(cols, rows) // I,J
+let oldP = zeros(cols, rows)
 let newP
 
 let tempX
@@ -31,9 +31,9 @@ const params = {
 
 function zeros (cols, rows) {
   let grid = []
-  for (let j = 0; j < cols; j++) {
+  for (let j = 0; j < rows; j++) {
     let line = []
-    for (let i = 0; i < rows; i++) {
+    for (let i = 0; i < cols; i++) {
       line.push(0)
     }
     grid.push(line)
@@ -42,8 +42,8 @@ function zeros (cols, rows) {
 }
 
 function couple (cArr, pArr, xArr, yArr) {
-  let iArr = zeros(xArr.length, xArr[0].length)
-  let jArr = zeros(yArr.length, yArr[0].length)
+  let iArr = zeros(cols + 1, rows)
+  let jArr = zeros(cols, rows + 1)
 
   // performs velocity calculation in x-axis
   for (let j = 1; j < iArr.length - 1; j++) {
@@ -60,7 +60,7 @@ function couple (cArr, pArr, xArr, yArr) {
         w: pArr[j][i - 1]
       }
 
-      const ox = vx.n * 5 + vx.s + vx.w + vx.e + (p.w - pArr[j][i]) * params.size // + divergence
+      const ox = vx.n + vx.s + vx.w + vx.e + (p.w - pArr[j][i]) * params.size // + divergence
 
       iArr[j][i] = ox
     }
@@ -87,26 +87,26 @@ function couple (cArr, pArr, xArr, yArr) {
     }
   }
 
-  for (let j = 0; j < cArr.length; j++) {
+  for (let j = 0; j < rows; j++) {
     let bool = {
       w: cArr[j][0] === 1,
-      e: cArr[j][cArr[j].length - 1] === 1
+      e: cArr[j][cols - 1] === 1
     }
 
-    jArr[j][0] += bool.w * 0.00005 // west wall
+    iArr[j][0] += bool.w * 0.00005 // west wall
 
-    jArr[j][cArr[j].length] -= bool.e * 0.00005 // east wall
+    iArr[j][cols] -= bool.e * 0.00005 // east wall
   }
 
-  for (let i = 0; i < cArr[0].length; i++) {
+  for (let i = 0; i < cols; i++) {
     let bool = {
       n: cArr[0][i] === 1,
-      s: cArr[cArr.length - 1][i] === 1
+      s: cArr[rows - 1][i] === 1
     }
 
-    iArr[0][i] += bool.n * 0.00005 // north wall
+    jArr[0][i] += bool.n * 0.00005 // north wall
 
-    iArr[cArr.length][i] -= bool.s * 0.00005 // south wall
+    jArr[rows][i] -= bool.s * 0.00005 // south wall
   }
 
   return {
@@ -116,7 +116,7 @@ function couple (cArr, pArr, xArr, yArr) {
 }
 
 function jacobi (cArr, pArr, xArr, yArr) {
-  let kArr = zeros(pArr.length, pArr[0].length)
+  let kArr = zeros(cols, rows)
 
   for (let j = 1; j < kArr.length - 1; j++) {
     for (let i = 1; i < kArr[j].length - 1; i++) {
@@ -142,7 +142,7 @@ function jacobi (cArr, pArr, xArr, yArr) {
   }
 
   // west side
-  for (let j = 1; j < kArr.length - 1; j++) {
+  for (let j = 1; j < rows - 1; j++) {
     const vx = {
       w: xArr[j][0],
       e: xArr[j][1]
@@ -163,7 +163,7 @@ function jacobi (cArr, pArr, xArr, yArr) {
   }
 
   // east wall
-  for (let j = 1; j < kArr.length - 1; j++) {
+  for (let j = 1; j < rows - 1; j++) {
     const vx = {
       w: xArr[j][kArr[j].length - 1],
       e: xArr[j][kArr[j].length]
@@ -177,14 +177,14 @@ function jacobi (cArr, pArr, xArr, yArr) {
     const p = {
       n: pArr[j - 1][kArr[j].length - 1],
       s: pArr[j + 1][kArr[j].length - 1],
-      w: pArr[j][kArr[j].length]
+      w: pArr[j][kArr[j].length - 2]
     }
 
-    kArr[j][kArr.length - 1] = p.n + p.s + p.w + (vx.w - vx.e + vy.n - vy.s)
+    kArr[j][kArr[j].length - 1] = p.n + p.s + p.w + (vx.w - vx.e + vy.n - vy.s)
   }
 
   // north edge
-  for (let i = 1; i < kArr[0].length - 1; i++) {
+  for (let i = 1; i < cols - 1; i++) {
     const vx = {
       w: xArr[0][i],
       e: xArr[0][i + 1]
@@ -205,7 +205,7 @@ function jacobi (cArr, pArr, xArr, yArr) {
   }
 
   // south side
-  for (let i = 1; i < kArr[0].length - 1; i++) {
+  for (let i = 1; i < cols - 1; i++) {
     const vx = {
       w: xArr[kArr.length - 1][i],
       e: xArr[kArr.length - 1][i + 1]
@@ -246,5 +246,6 @@ function execute () {
 }
 
 window.addEventListener('DOMContentLoaded', function () {
-  setInterval(execute, 20)
+//  setInterval(execute, 20)
+  execute()
 }, false)
