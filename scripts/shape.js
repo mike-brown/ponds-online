f2d725977a3a198e79020ac0a2da02d2e41d6a4f
Matt Barber <mbarber2@sheffield.ac.uk>
2017-06-22 15:28:54 +0100

    created: continued work on algorithm

--- scripts/shape.js
+++ scripts/shape.js
@@ -1,6 +1,20 @@
-var in_old = zeros(5, 5) // old values
-var in_new = zeros(5, 5) // new values
-var in_tmp = zeros(5, 5) // old values (preserved)
+var in_old = zeros(2, 2) // old values
+var in_new = zeros(2, 2) // new values
+var in_tmp = zeros(2, 2) // old values (preserved)
+
+
+
+in_new[0][0].velocity.x = 0.1
+in_new[0][0].velocity.y = 0.1
+
+in_new[0][1].velocity.x = 0
+in_new[0][1].velocity.y = 0
+
+in_new[1][0].velocity.x = 0
+in_new[1][0].velocity.y = 0
+
+in_new[1][1].velocity.x = 0.4
+in_new[1][1].velocity.y = 0.4
 
 function zeros(rows, cols) {
 
@@ -40,20 +54,22 @@ function advect(arr) {
 
 			let q12 = {
 				x: parseInt(Math.floor(x_old)),
-				y: parseInt(Math.ceil(y_old))
+				y: parseInt(Math.floor(y_old) + 1)
 			}
 
 			let q21 = {
-				x: parseInt(Math.ceil(x_old)),
+				x: parseInt(Math.floor(x_old) + 1),
 				y: parseInt(Math.floor(y_old))
 			}
 
 			let q22 = {
-				x: parseInt(Math.ceil(x_old)),
-				y: parseInt(Math.ceil(y_old))
+				x: parseInt(Math.floor(x_old) + 1),
+				y: parseInt(Math.floor(y_old) + 1)
 			}
 
-			out[x][y] = 
+			console.log(bilinear(arr, x, y, q11, q12, q21, q22))
+
+		//	out[x][y] = 
 		}
 	}
 }
@@ -65,18 +81,23 @@ function bilinear(arr, x, y, q11, q12, q21, q22) {
 		x1: q11.x,
 		y1: q11.y,
 		x2: q22.x,
-		y2: q22.y
+		y2: q22.y,
+
+		v11: border(arr, q11),
+		v12: border(arr, q12),
+		v21: border(arr, q21),
+		v22: border(arr, q22)
 	}
 
 	let term = 1 / (d.x2 - d.x1) * (d.y2 - d.y1)
 
 	// performs x-axis velocity computation of interpolation
-	let vx = ((d.x2 - d.x) * (arr[q11.x][q11.y].velocity.x + arr[q21.x][q21.y].velocity.x)) * (d.y2 - d.y)
-		   + ((d.x - d.x1) * (arr[q12.x][q12.y].velocity.x + arr[q22.x][q22.y].velocity.x)) * (d.y - d.y1)
+	let vx = (((d.x2 - d.x) * d.v11.x) + ((d.x - d.x1) * d.v21.x)) * (d.y2 - d.y)
+		   + (((d.x2 - d.x) * d.v12.x) + ((d.x - d.x1) * d.v22.x)) * (d.y - d.y1)
 
 	// performs y-axis velocity computation of interpolation
-	let vy = ((d.x2 - d.x) * (arr[q11.x][q11.y].velocity.y + arr[q21.x][q21.y].velocity.y)) * (d.y2 - d.y)
-		   + ((d.x - d.x1) * (arr[q12.x][q12.y].velocity.y + arr[q22.x][q22.y].velocity.y)) * (d.y - d.y1)
+	let vy = (((d.x2 - d.x) * d.v11.y) + ((d.x - d.x1) * d.v21.y)) * (d.y2 - d.y)
+		   + (((d.x2 - d.x) * d.v12.y) + ((d.x - d.x1) * d.v22.y)) * (d.y - d.y1)
 
 	return {
 		vx:  term * vx,
@@ -84,10 +105,24 @@ function bilinear(arr, x, y, q11, q12, q21, q22) {
 	}
 }
 
-function run() {
-	while (true) {
-
+function border(arr, q) {
+	let v = {
+		x: 0,
+		y: 0
 	}
+
+	if (q.x >= 0 && q.x < arr.length && q.y >= 0 && q.y < arr[0].length) {
+		v = {
+			x: arr[q.x][q.y].velocity.x,
+			y: arr[q.x][q.y].velocity.y
+		}
+	}
+
+	return v
+}
+
+function run() {
+	advect(in_new)
 }
 
 window.addEventListener("DOMContentLoaded", function() {
