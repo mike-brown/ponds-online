a3ec3d5d4c14c1dae8684c491ba1de9c8fa259f5
Matt Barber <mbarber2@sheffield.ac.uk>
2017-06-22 15:29:30 +0100

    modified: switched x-axis and y-axis usage

--- scripts/shape.js
+++ scripts/shape.js
@@ -26,9 +26,9 @@ const density = 1000.0 // 1000kg/m^3
 
 function zeros (rows, cols) {
   let grid = []
-  for (let x = 0; x < rows; x++) {
+  for (let y = 0; y < cols; y++) {
     let line = []
-    for (let y = 0; y < cols; y++) {
+    for (let x = 0; x < rows; x++) {
       line.push({
         pressure: 0, // pressure
         velocity: {
@@ -45,10 +45,10 @@ function zeros (rows, cols) {
 function advect (prev) {
   let arr = zeros(rows, cols)
 
-  for (let x = 0; x < prev.length; x++) {
-    for (let y = 0; y < prev[x].length; y++) {
-      let ox = x - prev[x][y].velocity.x * time
-      let oy = y - prev[x][y].velocity.y * time
+  for (let y = 0; y < prev.length; y++) {
+    for (let x = 0; x < prev[y].length; x++) {
+      let ox = x - prev[y][x].velocity.x * time
+      let oy = y - prev[y][x].velocity.y * time
 
       let q11 = {
         x: parseInt(Math.floor(ox)),
@@ -70,7 +70,7 @@ function advect (prev) {
         y: parseInt(Math.floor(oy)) + 1
       }
 
-      arr[x][y] = bilinear(prev, ox, oy, q11, q12, q21, q22)
+      arr[y][x] = bilinear(prev, ox, oy, q11, q12, q21, q22)
     }
   }
   return arr
@@ -119,15 +119,15 @@ function border (arr, q) {
 
   if (q.x >= 0 && q.x < arr.length && q.y >= 0 && q.y < arr[q.x].length) {
     v = {
-      p: arr[q.x][q.y].pressure,
-      x: arr[q.x][q.y].velocity.x,
-      y: arr[q.x][q.y].velocity.y
+      p: arr[q.y][q.x].pressure,
+      x: arr[q.y][q.x].velocity.x,
+      y: arr[q.y][q.x].velocity.y
     }
   }
 
   // outlet
-  if (q.x === 25 && q.y === 1) {
-    v.p = -1000
+  if (q.y === 25 && q.x === 1) {
+    v.p = -3000
   }
 
   return v
@@ -135,21 +135,21 @@ function border (arr, q) {
 
 function diverge (temp) {
   let grid = []
-  for (let x = 0; x < temp.length; x++) {
+  for (let y = 0; y < temp.length; y++) {
     let line = []
-    for (let y = 0; y < temp[x].length; y++) {
+    for (let x = 0; x < temp[y].length; x++) {
       // if not active, ignore
 
       let v = {
-        n: border(temp, { x: x - 1, y: y }),
-        e: border(temp, { x: x, y: y - 1 }),
-        s: border(temp, { x: x + 1, y: y }),
-        w: border(temp, { x: x, y: y + 1 })
+        n: border(temp, { x: x, y: y - 1 }),
+        e: border(temp, { x: x + 1, y: y }),
+        s: border(temp, { x: x, y: y + 1 }),
+        w: border(temp, { x: x - 1, y: y })
       }
 
       let term = (-2 * size * density) / time
 
-      line.push(term * (v.e.x - v.w.x + v.s.y - v.n.y)) // inverted x and y for axes
+      line.push(term * (v.e.x - v.w.x + v.s.y - v.n.y))
     }
     grid.push(line)
   }
@@ -159,23 +159,23 @@ function diverge (temp) {
 
 function jacobi (curr, prev, dvrg) {
   let arr = zeros(rows, cols)
-  for (let x = 0; x < prev.length; x++) {
-    for (let y = 0; y < prev[0].length; y++) {
+  for (let y = 0; y < prev.length; y++) {
+    for (let x = 0; x < prev[y].length; x++) {
       // if not active, ignore
 
       let p = {
-        n: border(prev, { x: x - 2, y: y }),
-        e: border(prev, { x: x, y: y - 2 }),
-        s: border(prev, { x: x + 2, y: y }),
-        w: border(prev, { x: x, y: y + 2 })
+        n: border(prev, { x: x, y: y - 2 }),
+        e: border(prev, { x: x + 2, y: y }),
+        s: border(prev, { x: x, y: y + 2 }),
+        w: border(prev, { x: x - 2, y: y })
       }
 
-      let term = (dvrg[x][y] + p.e.p + p.w.p + p.s.p + p.n.p) / 4
-      arr[x][y].pressure = term
+      let term = (dvrg[y][x] + p.e.p + p.w.p + p.s.p + p.n.p) / 4
+      arr[y][x].pressure = term
 
-      arr[x][y].velocity = {
-        x: curr[x][y].velocity.x,
-        y: curr[x][y].velocity.y
+      arr[y][x].velocity = {
+        x: curr[y][x].velocity.x,
+        y: curr[y][x].velocity.y
       }
     }
   }
@@ -184,28 +184,28 @@ function jacobi (curr, prev, dvrg) {
 
 function gradient (curr, prev) {
   let arr = zeros(rows, cols)
-  for (let x = 0; x < prev.length; x++) {
-    for (let y = 0; y < prev[x].length; y++) {
+  for (let y = 0; y < prev.length; y++) {
+    for (let x = 0; x < prev[y].length; x++) {
       // make current velocity using current pressure and past velocity
 
       // doesn't calculate if velocity cells aren't against a wall, unless designated as inlet or outlet
-      if ((x - 2 >= 0 && x + 2 < arr.length && y - 2 >= 0 && y + 2 < arr[0].length) || (x === 25 && y === 0) || ((x === 23 || x === 25 || x === 27) && y === cols - 1)) {
+      if ((x - 2 >= 0 && x + 2 < arr.length && y - 2 >= 0 && y + 2 < arr[0].length)) {
         let p = {
-          n: border(curr, { x: x - 1, y: y }), // velocity in negative x-axis (north)
-          e: border(curr, { x: x, y: y - 1 }), // velocity in negative y-axis (east)
-          s: border(curr, { x: x + 1, y: y }), // velocity in positive x-axis (south)
-          w: border(curr, { x: x, y: y + 1 }) // velocity in positive y-axis (west)
+          n: border(curr, { x: x, y: y - 1 }),
+          e: border(curr, { x: x + 1, y: y }),
+          s: border(curr, { x: x, y: y + 1 }),
+          w: border(curr, { x: x - 1, y: y })
         }
 
         let term = (time / (2 * size * density))
 
-        arr[x][y].velocity = {
-          x: prev[x][y].velocity.x - term * (p.e.p - p.w.p),
-          y: prev[x][y].velocity.y - term * (p.s.p - p.n.p)
+        arr[y][x].velocity = {
+          x: prev[y][x].velocity.x - term * (p.e.p - p.w.p),
+          y: prev[y][x].velocity.y - term * (p.s.p - p.n.p)
         }
       }
 
-      arr[x][y].pressure = curr[x][y].pressure
+      arr[y][x].pressure = curr[y][x].pressure
     }
   }
   return arr
@@ -222,18 +222,22 @@ function draw (arr) {
   let maxx = 0
   let maxy = 0
 
-  for (let x = 0; x < arr.length; x++) {
-    for (let y = 0; y < arr[0].length; y++) {
-      if (maxp < Math.abs(arr[x][y].pressure)) {
-        maxp = Math.abs(arr[x][y].pressure)
+  let sump = 0
+
+  for (let y = 0; y < arr.length; y++) {
+    for (let x = 0; x < arr[y].length; x++) {
+      sump += Math.abs(arr[y][x].pressure)
+
+      if (maxp < Math.abs(arr[y][x].pressure)) {
+        maxp = Math.abs(arr[y][x].pressure)
       }
 
-      if (maxx < Math.abs(arr[x][y].velocity.x)) {
-        maxx = Math.abs(arr[x][y].velocity.x)
+      if (maxx < Math.abs(arr[y][x].velocity.x)) {
+        maxx = Math.abs(arr[y][x].velocity.x)
       }
 
-      if (maxy < Math.abs(arr[x][y].velocity.y)) {
-        maxy = Math.abs(arr[x][y].velocity.y)
+      if (maxy < Math.abs(arr[y][x].velocity.y)) {
+        maxy = Math.abs(arr[y][x].velocity.y)
       }
     }
   }
@@ -242,22 +246,42 @@ function draw (arr) {
   let valx = 255.0 / maxx
   let valy = 255.0 / maxy
 
-  for (let x = 0; x < row; x++) {
-    for (let y = 0; y < col; y++) {
+  console.log('MAX:', maxp, maxx, maxy, 'SUM:', sump)
+
+  for (let y = 0; y < row; y++) {
+    for (let x = 0; x < col; x++) {
       ctxp.rect(x * 20 + 5, y * 20 + 5, 20, 20)
       ctxv.rect(x * 20 + 5, y * 20 + 5, 20, 20)
     }
   }
 
-  for (let x = 0; x < arr.length; x++) {
-    for (let y = 0; y < arr[x].length; y++) {
-      ctxv.fillStyle = 'rgb(' + parseInt(255 - valx * Math.abs(arr[x][y].velocity.x)) + ', ' + parseInt(255 - valy * Math.abs(arr[x][y].velocity.y)) + ', 255)'
+  for (let y = 0; y < arr.length; y++) {
+    for (let x = 0; x < arr[y].length; x++) {
+      ctxv.fillStyle = 'rgb(' + parseInt(255 - valx * Math.abs(arr[y][x].velocity.x)) + ', ' + parseInt(255 - valy * Math.abs(arr[y][x].velocity.y)) + ', 255)'
       ctxv.fillRect(x * 10, y * 10, 10, 10)
 
-      ctxp.fillStyle = 'rgb(' + parseInt(255 - valp * Math.abs(arr[x][y].pressure)) + ', ' + parseInt(255 - valp * Math.abs(arr[x][y].pressure)) + ', 255)'
+      ctxp.fillStyle = 'rgb(' + parseInt(255 - valp * Math.abs(arr[y][x].pressure)) + ', ' + parseInt(255 - valp * Math.abs(arr[y][x].pressure)) + ', 255)'
       ctxp.fillRect(x * 10, y * 10, 10, 10)
 
-      if (arr[x][y].velocity.x > 0) {
+      if (arr[y][x].pressure > 0) {
+        ctxp.beginPath()
+        ctxp.moveTo(x * 10, y * 10 + 10)
+        ctxp.lineTo(x * 10 + 5, y * 10)
+        ctxp.lineTo(x * 10 + 10, y * 10 + 10)
+        ctxp.closePath()
+        ctxp.stroke()
+      }
+
+      if (arr[y][x].pressure < 0) {
+        ctxp.beginPath()
+        ctxp.moveTo(x * 10, y * 10)
+        ctxp.lineTo(x * 10 + 5, y * 10 + 10)
+        ctxp.lineTo(x * 10 + 10, y * 10)
+        ctxp.closePath()
+        ctxp.stroke()
+      }
+
+      if (arr[y][x].velocity.y < 0) {
         ctxv.beginPath()
         ctxv.moveTo(x * 10, y * 10 + 10)
         ctxv.lineTo(x * 10 + 5, y * 10)
@@ -266,7 +290,7 @@ function draw (arr) {
         ctxv.stroke()
       }
 
-      if (arr[x][y].velocity.x < 0) {
+      if (arr[y][x].velocity.y > 0) {
         ctxv.beginPath()
         ctxv.moveTo(x * 10, y * 10)
         ctxv.lineTo(x * 10 + 5, y * 10 + 10)
@@ -275,7 +299,7 @@ function draw (arr) {
         ctxv.stroke()
       }
 
-      if (arr[x][y].velocity.y > 0) {
+      if (arr[y][x].velocity.x < 0) {
         ctxv.beginPath()
         ctxv.moveTo(x * 10 + 10, y * 10)
         ctxv.lineTo(x * 10, y * 10 + 5)
@@ -284,7 +308,7 @@ function draw (arr) {
         ctxv.stroke()
       }
 
-      if (arr[x][y].velocity.y < 0) {
+      if (arr[y][x].velocity.x > 0) {
         ctxv.beginPath()
         ctxv.moveTo(x * 10, y * 10)
         ctxv.lineTo(x * 10 + 10, y * 10 + 5)
@@ -305,24 +329,24 @@ function run () {
   temp = advect(prev)
 
   // inlet
-  temp[23][cols - 1].velocity.x = -1.0
-  temp[25][cols - 1].velocity.x = -1.0
-  temp[27][cols - 1].velocity.x = -1.0
+  temp[23][cols - 1].velocity.x = -0.1
+  temp[25][cols - 1].velocity.x = -0.1
+  temp[27][cols - 1].velocity.x = -0.1
 
   curr = gradient(jacobi(curr, prev, diverge(temp)), prev)
   draw(curr)
 
-  for (let x = 0; x < prev.length; x++) {
-    for (let y = 0; y < prev[x].length; y++) {
-      prev[x][y].pressure = curr[x][y].pressure
-      prev[x][y].velocity = {
-        x: curr[x][y].velocity.x,
-        y: curr[x][y].velocity.y
+  for (let y = 0; y < prev.length; y++) {
+    for (let x = 0; x < prev[y].length; x++) {
+      prev[y][x].pressure = curr[y][x].pressure
+      prev[y][x].velocity = {
+        x: curr[y][x].velocity.x,
+        y: curr[y][x].velocity.y
       }
     }
   }
 }
 
 window.addEventListener('DOMContentLoaded', function () {
-  setInterval(run, 20)
+  setInterval(run, 50)
 }, false)
